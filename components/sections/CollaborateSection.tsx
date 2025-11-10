2ccc4859329d5b510b659646115dd0018c1ccef0
SeniduRavihara <senidumee16@gmail.com>
2025-11-10 11:27:07 +0530

    update logo

--- components/sections/CollaborateSection.tsx
+++ components/sections/CollaborateSection.tsx
@@ -5,7 +5,7 @@ const CollaborateSection: React.FC = () => {
   return (
     <section className="py-16 px-4">
       <div className="max-w-4xl mx-auto text-center">
-        <p className="text-lg mb-4">collaborate with</p>
+        <p className="text-5xl mb-4">collaborate with</p>
         <div className="bg-gray-800 p-4 rounded-lg inline-block mx-auto">
           <Image
             src="/SLAAIlogo.jpg"
